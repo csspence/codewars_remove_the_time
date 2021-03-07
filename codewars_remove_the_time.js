c3e8f04ead8b8d3d890e37e3a16b0eea5114b1e3
Crew Spence <csspence123@gmail.com>
2021-03-07 07:13:59 -0600

    Still working on this.  Ran out of time.

--- codewars_remove_the_time.js
+++ codewars_remove_the_time.js
@@ -10,6 +10,18 @@ Write a function, shortenToDate, that takes the Website date/time in its origina
 Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
 */
 
-function shortenToDate(longDate) {
-  // your code here
+const shortenToDate = (longDate) => {
+  let subtract;
+  let spaces = 0
+  for(let i = longDate.length; i > long; i--) {
+    if(longDate[i] !== ',') {
+      spaces++;
+    }
+    if(longDate[i] === ',') {
+      spaces++;
+      break;
+    }
+  }
+
+  return longDate.slice(0, subtract);
 }
